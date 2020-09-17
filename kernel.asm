7148033052174d89dd62321e75299094ae36201f
Karen <karenl.harper88@gmail.com>
2020-09-16 23:50:24 -0700

    Flesh out kernel

--- kernel.asm
+++ kernel.asm
@@ -1,33 +1,26 @@
   processor 6502
   include "./dasm-src/machines/atari2600/vcs.h" ;Definitions for TIA register names
+  include "./dasm-src/machines/atari2600/macro.h" ;DASM Macro definitiom
   
   org 0
 
+Reset; System Reset 
+
   ldx #0 
-  ldy #0 
+  lda #0 
 
-  REPEAT 192; 
-
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
+Clear ;Initialize TIA and RAM
+    
+  sta 0,x
+  inx
+  bne Clear
+  
+StartOfFrame
 
   inx 
   stx COLUBK 
 
-  NOP
-  NOP
-  NOP
-
-  dey
+  ldy #F
   sty COLUBK
 
   sta WSYNC
