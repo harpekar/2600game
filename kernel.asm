1607c2e7c7841a256ea1efc60cace31e54cd185f
Karen <karenl.harper88@gmail.com>
2020-09-17 23:53:46 -0700

    Complete kernel

--- kernel.asm
+++ kernel.asm
@@ -1,7 +1,10 @@
   processor 6502
   include "./dasm-src/machines/atari2600/vcs.h" ;Definitions for TIA register names
   include "./dasm-src/machines/atari2600/macro.h" ;DASM Macro definitiom
-  
+ 
+PATTERN = $80 ;storage location
+TIMETOCHANGE = 20 ;speed of animation
+
   seg
   org $F000
 
@@ -10,20 +13,88 @@ Reset; System Reset
   ldx #0 
   lda #0 
 
-Clear ;Initialize TIA and RAM
-    
-  sta 0,x
+Clear sta 0,x ;Initialize TIA and RAM
   inx
   bne Clear
   
-StartOfFrame
+  lda #0
+  sta PATTERN ;Binary PF pattern
 
-  inx 
-  stx COLUBK 
+  lda #$45
+  sta COLUPF ;Playfield color
 
   ldy #0
-  sty COLUBK
+
+StartOfFrame
+
+  lda #0
+  sta VBLANK
+
+  lda #2
+  sta VSYNC
 
   sta WSYNC
+  sta WSYNC
+  sta WSYNC  
 
-  REPEND
+  lda #0
+  sta VSYNC
+
+  ;vertical blank
+
+  ldx #0
+
+VerticalBlank sta WSYNC
+  
+  inx
+  cpx #37
+
+  bne VerticalBlank
+
+  ;change in pattern every 20 frames
+
+  iny 
+  cpy #TIMETOCHANGE
+
+  bne keepsame
+
+  ldy #0
+  inc PATTERN
+
+keepsame
+  lda PATTERN ;use saved pattern in playfield
+  sta PF1
+
+  ldx #0
+
+Picture stx COLUBK ;
+  sta WSYNC ;wait until end of scanline
+
+  inx 
+  cpx #192
+
+  bne Picture
+
+  lda #%01000010
+
+  sta VBLANK
+  ldx #0
+
+Overscan sta WSYNC
+  inx 
+  cpx #30
+  bne Overscan
+
+  jmp StartOfFrame
+
+  ;--------------------
+
+  org $FFFA
+
+InterruptVectors
+  
+  .word Reset ; NMI
+  .word Reset ; RESET
+  .word Reset ; IRQ
+
+  END
